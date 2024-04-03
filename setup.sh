338a319547892ddb1ad125b59cad0d101118441e
Ryan C <963378+ryanch94@users.noreply.github.com>
2024-04-03 22:18:59 +0100

    Merge apt install commands

--- setup.sh
+++ setup.sh
@@ -4,33 +4,12 @@
 echo "Installing curl..."
 sudo apt install curl -y
 
-# Install Brave
-echo "Installing Brave..."
+# Add Brave repo to apt sources
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
 sudo apt update
-sudo apt install brave-browser -y
-
-# Install calibre
-sudo apt install calibre -y
-
-# Install filezilla
-sudo apt install filezilla -y
-
-# Install build-essentials
-sudo apt install build-essentials -y
-
-# Install thunderbird
-sudo apt install thunderbird -y
-
-# Install libreoffice
-sudo apt install libreoffice -y
-
-# Install vlc
-sudo apt install vlc -y
-
-# Install wine
-sudo apt install wine -y
+sudo apt install brave-browser calibre filezilla build-essentials thunderbird libreoffice vlc wine -y
 
 # Update packages
 echo "Upgrading Packages..."
